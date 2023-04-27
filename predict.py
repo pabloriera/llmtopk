38e8afb2dd139cdc46aa748bd894abc8161cc09a
Pablo Riera <pablo.riera@gmail.com>
2023-04-27 01:42:34 -0300

    up

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 import numpy as np
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, StoppingCriteriaList
 model_name = "vicuna-7B-1.1-HF"
 
 model = AutoModelForCausalLM.from_pretrained(
@@ -14,23 +14,28 @@ print("Model loaded")
 def predict(query: str, max_tokens: str, top_k: int):
     generation_config = GenerationConfig(
         num_beams=1,
+        do_sample=True,
         top_k=top_k,
         early_stopping=True,
-        max_new_tokens=max_tokens
+        max_new_tokens=50,
+        temperature=0.8,
+        eos_token_id=model.config.eos_token_id,
     )
     model.generation_config = generation_config
-
     inputs = tokenizer(
         query, return_tensors="pt").input_ids.to("cuda")
     outputs = model.generate(
-        inputs, return_dict_in_generate=True, output_scores=True)
+        inputs,
+        return_dict_in_generate=True,
+        output_scores=True)
     transition_scores = model.compute_transition_scores(
         outputs.sequences, outputs.scores,  normalize_logits=True
     )
     input_length = 1 if model.config.is_encoder_decoder else inputs.shape[1]
     generated_tokens = outputs.sequences[:, input_length:]
-    tokens = [tokenizer.decode(v) for v in generated_tokens[0]]
+    tokens = [tokenizer.decode(v) for v in generated_tokens[0] if v > 2]
     string = tokenizer.decode(generated_tokens[0])
+    string = string.replace('</s>', '').replace('<s>', '')
     probs = np.exp(transition_scores.detach().cpu().numpy())[0]
 
     return tokens, string, probs
